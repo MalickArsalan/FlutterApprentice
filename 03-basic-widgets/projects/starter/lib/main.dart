63ae06bdd5cb1f165e0b1173f40b2d7d337c9166
Malick Arsalan <malickarsalan@hotmail.com>
2021-10-24 23:49:51 +0500

    3. Basic Widgets(Initializing)

---
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+// 1
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    final theme = FooderlichTheme.light();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // 3
+      theme: theme,
+      title: 'Fooderlich',
+      home: const Home(),
+    );
+  }
+}
