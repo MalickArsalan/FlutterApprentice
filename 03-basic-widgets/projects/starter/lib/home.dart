df9b991b9de6b560b190299f7d6042ec12c8655b
Malick Arsalan <malickarsalan@hotmail.com>
2021-10-26 07:27:29 +0500

    App structure and navigation

---
+++ 03-basic-widgets/projects/starter/lib/home.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+// 1
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // ignore: todo
+  // TODO: Add state variables and functions
+  // 7
+  int _selectedIndex = 0;
+
+  // 8
+  static List<Widget> pages = <Widget>[
+    // TODO: Replace with Card1
+    Container(color: Colors.red),
+    // TODO: Replace with Card2
+    Container(color: Colors.green),
+    // TODO: Replace with Card3
+    Container(
+      color: Colors.blue,
+    )
+  ];
+
+  // 9
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Fooderlich',
+          // 2
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      // ignore: todo
+      // TODO: Show selected tab
+      body: pages[_selectedIndex],
+      // ignore: todo
+      // TODO: Add bottom navigation bar
+      // 4
+      bottomNavigationBar: BottomNavigationBar(
+        // 5
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // ignore: todo
+        // TODO: Set selected tab bar
+        // 10
+        currentIndex: _selectedIndex,
+        // 11
+        onTap: _onItemTapped,
+
+        // 6
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // 5
 import 'fooderlich_theme.dart';
+import 'home.dart';
 
 void main() {
   // 1
@@ -16,34 +17,21 @@ class Fooderlich extends StatelessWidget {
     // TODO: Create theme
     // 6
     final theme = FooderlichTheme.dark();
-    // ignore: todo
-    // TODO: Apply Home widget
-    // 3
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       // ignore: todo
       // TODO: Add theme
       // 7
       theme: theme,
       title: 'Fooderlich',
+
       // 4
-      home: Scaffold(
-        // ignore: todo
-        // TODO: Style the title
-        appBar: AppBar(
-            title: Text(
-          'Fooderlich',
-          // 8
-          style: theme.textTheme.headline6,
-        )),
-        // ignore: todo
-        // TODO: Style the body text
-        body: Center(
-            child: Text(
-          'Let\'s get cooking 👩‍🍳',
-          // 9
-          style: theme.textTheme.headline1,
-        )),
-      ),
+      // ignore: todo
+      // TODO: Apply Home widget
+      // 3
+      home: Home(),
     );
   }
 }
