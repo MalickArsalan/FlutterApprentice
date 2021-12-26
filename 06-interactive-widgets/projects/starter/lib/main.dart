a7ab2fc5ba9e9f624e1207cc0f116761948d3b46
Malick Arsalan <malickarsalan@hotmail.com>
2021-12-26 08:47:33 +0500

    Interactive Widgets Getting started

---
+++ 06-interactive-widgets/projects/starter/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      // ignore: todo
+      // TODO 8: Replace this with MultiProvider
+      home: const Home(),
+    );
+  }
+}
