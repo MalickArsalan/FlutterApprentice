f2fb501522518dd6a1283d0dc3d8e6c4603eab8f
Malick Arsalan <malickarsalan@hotmail.com>
2021-12-27 03:59:48 +0500

    Switching Tabs and Managing Tabs (Completed)

--- 06-interactive-widgets/projects/starter/lib/main.dart
+++ 06-interactive-widgets/projects/starter/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
 import 'home.dart';
 
+import 'package:provider/provider.dart';
+import 'models/models.dart';
+
 void main() {
   runApp(const Fooderlich());
 }
@@ -14,11 +17,20 @@ class Fooderlich extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.light();
     return MaterialApp(
-      theme: theme,
-      title: 'Fooderlich',
-      // ignore: todo
-      // TODO 8: Replace this with MultiProvider
-      home: const Home(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: theme,
+        title: 'Fooderlich',
+        // ignore: todo
+        // TODO 8: Replace this with MultiProvider
+        //home: const Home(),
+        // 1
+        home: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (context) => TabManager())
+            // ignore: todo
+            // TODO 10: Add GroceryManager Provider
+          ],
+          child: const Home(),
+        ));
   }
 }
