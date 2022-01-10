86ae22204b0548783429eb007d978214e3839928
MalickArsalan Home <malickarsalan@hotmail.com>
2022-01-10 22:45:34 +0500

    Saving User’s Work

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_item_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_item_screen.dart
@@ -91,6 +91,26 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
             onPressed: () {
               // ignore: todo
               // TODO 24: Add callback handler
+              // 1
+              final groceryItem = GroceryItem(
+                id: widget.originalItem?.id ?? const Uuid().v1(),
+                name: _nameController.text,
+                importance: _importance,
+                color: _currentColor,
+                quantity: _currentSliderValue,
+                date: DateTime(
+                  _dueDate.year,
+                  _dueDate.month,
+                  _dueDate.day,
+                  _timeOfDay.hour,
+                  _timeOfDay.minute,
+                ),
+              );
+              if (widget.isUpdating) {
+                widget.onUpdate(groceryItem);
+              } else {
+                widget.onCreate(groceryItem);
+              }
             },
           ),
         ],
