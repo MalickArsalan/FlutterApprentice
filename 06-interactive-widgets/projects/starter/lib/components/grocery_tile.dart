5b597b5cff29ca92abc6bb8d5ab9e10c854544be
MalickArsalanOffice <malickarsalan@hotmail.com>
2022-01-10 10:57:37 +0500

    Building GroceryTile(Display Selected Date)

--- 06-interactive-widgets/projects/starter/lib/components/grocery_tile.dart
+++ 06-interactive-widgets/projects/starter/lib/components/grocery_tile.dart
@@ -63,6 +63,11 @@ class GroceryTile extends StatelessWidget {
 
   // ignore: todo
   // TODO: Add buildDate()
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateString = dateFormatter.format(item.date);
+    return Text(dateString, style: TextStyle(decoration: textDecoration));
+  }
 
   // ignore: todo
   // TODO: Add buildCheckbox()
